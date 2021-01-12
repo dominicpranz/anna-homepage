848233ded8bda07613a5631cfdd76ffb053ffdb7
Dominic <dominic.pr93@gmail.com>
2021-01-13 00:00:32 +0100

    added tailwind and a test page

---
+++ pages/test.vue
@@ -0,0 +1,15 @@
+!<template>
+  <div class="test">
+    <h1 class="text-5xl text-green-700 lg:text-red-800">123</h1>
+  </div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+.test {
+  h1 {
+    //color: blue;
+  }
+}
+</style>
